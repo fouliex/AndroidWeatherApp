c364bf3132f98436dd53b70b5172636aef0649e0
George Fouche <fouliex@gmail.com>
2016-05-21 17:53:22 -0500

    Added menu

--- app/src/main/java/com/fouliex/android/weather/MainActivity.java
+++ app/src/main/java/com/fouliex/android/weather/MainActivity.java
@@ -2,6 +2,8 @@ package com.fouliex.android.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id =item.getItemId();
+        if(id==R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
